062dd830b1e191c6cf9f6ef9135f378d1951ec1f
w181496 <w181496@gmail.com>
2017-11-02 15:13:58 +0800

    update

--- EasySolver.rb
+++ EasySolver.rb
@@ -5,15 +5,17 @@ require 'colorize'
 
 $host = 'localhost'
 $path = '/index.php'
+$port = 80
 $keyword = 'CTF'
 
 ARGV.each do |arg|
     $host = arg.split("=")[1] if arg.include?("--host=")
     $path = arg.split("=")[1] if arg.include?("--path=")
+    $port = arg.split("=")[1] if arg.include?("--port=")
     $keyword = arg.split("=")[1] if arg.include?("--key=")
 end
 
-res = Net::HTTP.get_response($host, $path)
+res = Net::HTTP.get_response($host, $path, $port)
 
 puts "===========header===========".colorize(:blue)
 URI.unescape(res.to_hash.to_s).scan(/#{$keyword}{([^}]*)}/) {|str| puts "#{$keyword}{#{str[0].to_s}}".colorize(:green)}
@@ -27,7 +29,8 @@ puts "============================".colorize(:blue)
 
 puts "\n"
 
-res2 = Net::HTTP.get_response($host, '/robots.txt')
+# robots.txt
+res2 = Net::HTTP.get_response($host, '/robots.txt', $port)
 if res2.code == '200'
     puts "=========robots.txt=========".colorize(:blue)
     puts res2.body
@@ -36,24 +39,102 @@ end
 
 puts "\n"
 
-res2 = Net::HTTP.get_response($host, '/.git/')
-if res2.code != '404'
-    puts "=============git============".colorize(:blue)
-    puts ".git Found!".colorize(:yellow)
+# .DS_Store
+res2 = Net::HTTP.get_response($host, '/.DS_Store', $port)
+if res2.code == '200'
+    puts "==========.DS_Store=========".colorize(:blue)
+    puts ".DS_Store Found!".colorize(:yellow)
     puts "============================".colorize(:blue)
 end
 
 puts "\n"
 
 
+# .git/
+res2 = Net::HTTP.get_response($host, '/.git/', $port)
+if res2.code != '404'
+    puts "=============git============".colorize(:blue)
+    puts ".git/ Found!".colorize(:yellow)
+    puts "============================".colorize(:blue)
+end
+
+puts "\n"
+
+# .svn/
+res2 = Net::HTTP.get_response($host, '/.svn/', $port)
+if res2.code != '404'
+    puts "=============svn============".colorize(:blue)
+    puts ".svn/ Found!".colorize(:yellow)
+    puts "============================".colorize(:blue)
+end
+
+
+puts "\n"
+
+# WEB-INF/
+res2 = Net::HTTP.get_response($host, '/WEB-INF/', $port)
+if res2.code != '404'
+    puts "===========WEB-INF==========".colorize(:blue)
+    puts "WEB-INF/ Found!".colorize(:yellow)
+    puts "============================".colorize(:blue)
+end
+
+puts "\n"
+
+# admin/
+res2 = Net::HTTP.get_response($host, '/admin/', $port)
+if res2.code != '404'
+    puts "=============git============".colorize(:blue)
+    puts "admin/ Found!".colorize(:yellow)
+    puts "============================".colorize(:blue)
+end
+
+puts "\n"
+
+# login/
+res2 = Net::HTTP.get_response($host, '/login/', $port)
+if res2.code != '404'
+    puts "============login===========".colorize(:blue)
+    puts "login/ Found!".colorize(:yellow)
+    puts "============================".colorize(:blue)
+end
+
+puts "\n"
+
+# phpMyAdmin/ phpmyadmin/ pma/
+res2 = Net::HTTP.get_response($host, '/phpMyAdmin/', $port)
+if res2.code != '404'
+    puts "==========phpMyAdmin========".colorize(:blue)
+    puts "phpMyAdmin/ Found!".colorize(:yellow)
+    puts "============================".colorize(:blue)
+end
+
+res2 = Net::HTTP.get_response($host, '/phpmyadmin/', $port)
+if res2.code != '404'
+    puts "==========phpmyadmin========".colorize(:blue)
+    puts "phpmyadmin/ Found!".colorize(:yellow)
+    puts "============================".colorize(:blue)
+end
+
+res2 = Net::HTTP.get_response($host, '/pma/', $port)
+if res2.code != '404'
+    puts "=============pma============".colorize(:blue)
+    puts "pma/ Found!".colorize(:yellow)
+    puts "============================".colorize(:blue)
+end
+
+puts "\n"
+
+
+
 URI.unescape(res.body).scan(/\"([^\"]*).js\"/) do |str| 
     puts "#{str[0].to_s}.js Found!".colorize(:yellow)
-    tmp_res = Net::HTTP.get_response($host, "/#{str[0].to_s}.js")
+    tmp_res = Net::HTTP.get_response($host, "/#{str[0].to_s}.js", $port)
     URI.unescape(tmp_res.body).scan(/#{$keyword}{([^}]*)}/) {|str| puts "#{$keyword}{#{str[0].to_s}}".colorize(:green)}
 end
 
 URI.unescape(res.body).scan(/\"([^\"]*).css\"/) do |str| 
     puts "#{str[0].to_s}.css Found!".colorize(:yellow)
-    tmp_res = Net::HTTP.get_response($host, "/#{str[0].to_s}.css")
+    tmp_res = Net::HTTP.get_response($host, "/#{str[0].to_s}.css", $port)
     URI.unescape(tmp_res.body).scan(/#{$keyword}{([^}]*)}/) {|str| puts "#{$keyword}{#{str[0].to_s}}".colorize(:green)}
 end
--- README.md
+++ README.md
@@ -2,15 +2,17 @@
 
 This tool is to solve CTF web baby first problem.
 
-It will check robots.txt、.git、js、css、cookie、response header and body.
+It will check robots.txt、.git/.svn、phpmyadmin、js、css、cookie、response header and body.
 
-I don't like easy web problems :(
+I'm so lazy. I don't like easy web problems :(
 
 ## How to use
 
 find FLAG{xxxx}: `ruby EasySolver.rb --host=www.ctf.tw --key=FLAG`
 
-find CTF{xxxx}: `ruby EasySolver.rb --host=kaibro.tw --key=CTF`
+find CTF{xxxx}: `ruby EasySolver.rb --host=ctf.kaibro.tw --key=CTF`
+
+setting port: `ruby EasySolver.rb --host=ctf.kaibro.tw --key=FLAG --port=7788`
 
 ## Installation
 
